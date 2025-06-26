6fb7225882f12a36f1b6d6ca197fb646c1b987da
Ghostdog <ajhallett98@gmail.com>
2025-06-25 19:18:20 -0500

    Add logic for getHumanChoice
    Added a if statement that takes a player's input and outputs a statement
    in the log.

--- script.js
+++ script.js
@@ -2,16 +2,34 @@
 // Player manually types in their option.
 // program needs to know which combinations win or lose
 // Program needs to keep track of wins
-const hands = [ "Rock", "Paper", "Scissors" ];
+const hands = [ "rock", "paper", "scissors" ];
+
+const humanScore = 0
+const computerScore = 0
+
+
 
 function getComputerChoice(max) {
     const randomHand = Math.floor(Math.random() * max);
     return hands [randomHand];
 }
 
-function getHumanChoice() {
 
+function getHumanChoice() {
+    let sign = prompt("Test your luck!")
+
+    if (sign === "") {
+        console.log("You have to put something!")
+    } else if (sign.toLowerCase() === "rock") {
+        console.log("Solid Choice!")
+    } else if (sign.toLowerCase() === "scissors"){
+        console.log("You're Sharp!")
+    } else if (sign.toLowerCase() === "paper") {
+        console.log("Crispy.")
+    } else {console.log("That's not an option...")}  
 }
+
+getHumanChoice();
 
 function playRound(humanChoice,computerChoice) {
 
